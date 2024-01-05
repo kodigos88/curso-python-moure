66641dc2c57a05a9573f9189a61ed2b7ca25518c
kodigos88 <kodigosat@gmail.com>
2024-01-05 10:12:20 -0400

    primeras 2 clases de python

---
+++ 00_hello.py
@@ -0,0 +1,16 @@
+# mi primer codigo en PYTHON
+print ("Hola mundo hecho en python")
+print('Hola mundo en python')
+
+"""
+Esto es un 
+comentario de 
+varias lineas
+"""
+
+# Consultar los tipos de datos
+print(type("soy un str")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(1.8)) # Tipo 'float'
+print(type(3 + 8j)) # Tipo 'complex'
+print(type(True)) # Tipo 'bool'
